d507234a43f1316d18ed9d0a0b1d4833f1744400
stephenthoen1 <sthoen16@my.whitworth.edu>
2015-01-05 23:36:44 -0800

    1/5/15 HomeWork
    Everything except 3 and 6

---
+++ HW1/src/hw1/AreaFormual.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw1;
+
+/**
+ *
+ * @author stephenthoen
+ */
+public class AreaFormual {
+    public static double PI = 3.14; 
+    
+    public static double area(double a)
+    {
+        return PI*Math.pow(a, 2);
+    };
+}
---
+++ HW1/src/hw1/RandomNumbers.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw1;
+import java.util.*;
+/**
+ *
+ * @author stephenthoen
+ */
+public class RandomNumbers {
+    
+    public void randtest()
+    {
+        Random rand = new Random();
+        int firstrand = rand.nextInt(100)+1;
+        int secrand = rand.nextInt(100)+1;
+
+        if(firstrand == secrand)
+        {
+            System.out.printf("%d is equal to %d\n", firstrand, secrand);
+        }
+        else if(firstrand > secrand)
+        {
+            System.out.printf("%d is Greater Than %d\n", firstrand, secrand);
+        }
+        else if(firstrand < secrand)
+        {
+            System.out.printf("%d is Less Than %d\n", firstrand, secrand);
+        }
+    };
+    
+}
